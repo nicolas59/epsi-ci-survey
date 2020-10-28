7c1afddbd7ad489d4ba7f6d1192ebe8fbe39b9d5
Nicolas Rousseau <nicolas.rousseau1@gfi.fr>
2020-10-28 23:12:22 +0100

    disabled cors

---
+++ src/main/java/fr/nro/interview/filter/CorsFilter.java
@@ -0,0 +1,20 @@
+package fr.nro.interview.filter;
+
+import javax.ws.rs.container.ContainerRequestContext;
+import javax.ws.rs.container.ContainerResponseContext;
+import javax.ws.rs.container.ContainerResponseFilter;
+import javax.ws.rs.ext.Provider;
+import java.io.IOException;
+
+
+@Provider
+public class CorsFilter implements ContainerResponseFilter {
+
+    @Override
+    public void filter(ContainerRequestContext requestContext, ContainerResponseContext responseContext) throws IOException {
+        responseContext.getHeaders().add("Access-Control-Allow-Origin", "*");
+        responseContext.getHeaders().add("Access-Control-Request-Method", "POST, GET, OPTIONS, DELETE");
+        responseContext.getHeaders().add("Access-Control-Allow-Headers", "*");
+
+    }
+}
